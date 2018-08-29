96c64b7a4ce14a78d3ce655ba88ddf3493eb8e28
KiNGPiN <sfy@mail.ru>
2018-08-30 02:48:53 +0300

    Файлы, создаваемые для публикации, уменьшаются в размере

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,39 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
 // создает карту исходника для отладки
 const sourcemaps = require("gulp-sourcemaps");
-// преобразует js в команды для браузеров без поддержки ES-2015
+// преобразует js в команды для браузеров без поддержки ES-2015 (преобразуемые файлы находятся в html-коде между комментариями build и endbuild)
 const babel = require("gulp-babel");
 // объединяет файлы
-const concat = require("gulp-concat");
+const useref = require("gulp-useref");
 // сервер с поддержкой обновления страниц
-const browserSync = require('browser-sync').create();
+const browserSync = require("browser-sync").create();
 const refresh = browserSync.reload;
 // преобразует scss в css
-const sass = require('gulp-sass');
-const browserify = require('browserify');
-const uglify = require('gulp-uglify');
+const sass = require("gulp-sass");
+// Уменьшает размер JS-файла
+const uglify = require("gulp-uglify");
+// Используется для изменения CSS файлов
+const postcss = require('gulp-postcss');
+// Добавляет в CSS файлы префиксы для поддержки разных браузеров
+const autoprefixer = require('autoprefixer');
+// Уменьшает размер CSS-файла
+const csso = require('gulp-csso');
+// Включает поддержку ветвлений в gulp
+const gulpIf = require("gulp-if");
 
 // Компилирует sass в CSS и обновляет страницу в браузере
 gulp.task("sass", () => {
+    let plugins = [
+        // добавляем префиксы для поддержки разных браузеров
+        autoprefixer(),
+    ];
     // берем scss
     return gulp.src("app/scss/*.scss")
         // преобразуем в css
         .pipe(sass())
         // создаем карту исходника для отладки
         .pipe(sourcemaps.init())
+        .pipe(postcss(plugins))
         .pipe(sourcemaps.write("."))
         // сохраняем css
         .pipe(gulp.dest("app/css"))
@@ -36,8 +49,6 @@ gulp.task("oldjs", function () {
         .pipe(babel({
             presets: ['@babel/env']
         }))
-        // соединяем все js файлы в 1
-        .pipe(concat("site.js"))
         // сохраняем карту исходника для отладки
         .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("app/js"))
@@ -59,23 +70,17 @@ gulp.task("devserver", () => {
 // Запускает сервер для разработки
 gulp.task("default", gulp.series("sass", "oldjs", "devserver"));
 
-// Создает js-файлы для публикации на сервере
-gulp.task("js", () => {
-    return gulp.src("app/js/site.js")
-        .pipe(gulp.dest("dist/js"));
-});
-// Создает html-файлы для публикации на сервере
-gulp.task("html", () => {
+// Создает файлы для публикации на сервере
+gulp.task("dist", () => {
+    // соединяем все js и css в 2 отдельных файла
     return gulp.src("app/*.html")
+        .pipe(useref())
+        // Уменьшаем CSS-файл
+        .pipe(gulpIf("*.css", csso()))
+        // Уменьшаем JS-файл
+        .pipe(gulpIf("*.js", uglify()))
         .pipe(gulp.dest("dist"));
 });
-// Создает css-файлы для публикации на сервере
-gulp.task("css", () => {
-    return gulp.src("app/css/*.css")
-        .pipe(gulp.dest("dist/css"));
-});
-// Создает файлы для публикации на сервере
-gulp.task("dist", gulp.series('js', 'html', 'css'));
 // Создает статический сервер с контролем изменений в файлах scss/html
 gulp.task("official", () => {
     browserSync.init({
@@ -89,4 +94,4 @@ gulp.task("official", () => {
     gulp.watch("dist/*.html").on("change", refresh);
 });
 // Запускает сервер для разработки
-gulp.task("production", gulp.series('dist', 'official'));
+gulp.task("production", gulp.series("dist", "official"));
